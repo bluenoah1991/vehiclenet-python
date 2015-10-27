2f6bb1875d99fa6bece4b6f48847b400ad4210c0
CodeMeow <codemeow@yahoo.com>
2015-10-27 18:13:18 +0800

    Today is the last day of your life so far.

---
+++ vehiclenet/weather.py
@@ -0,0 +1,141 @@
+#!/usr/bin/python
+
+import sys, os
+import tornado.web
+import urllib2
+import logging
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+API_TOKEN = 'FSUZWU8RKI'
+WEATHER_API_URI = 'https://api.thinkpage.cn/v2/weather/all.json?language=zh-chs&unit=c&aqi=city&key=FSUZWU8RKI&city=%s'
+
+logger = logging.getLogger('web')
+
+class WeatherHandler(tornado.web.RequestHandler):
+	def get(self):
+
+		logger.debug('request info') # TODO
+
+		city_name = self.get_argument('city')
+		if city_name is None or len(city_name) == 0:
+			self.write(201)
+			return
+		city_name = self.name_parse(city_name)
+		content_from_api = None
+		try:
+			response = urllib2.urlopen(WEATHER_API_URI % city_name)
+			content_from_api = response.read()
+			response.close()
+		except Exception, e:
+			logger.error('HTTP request error, error: (%s)' % e)
+			self.write(501)
+			return
+		if content_from_api is None or len(content_from_api) == 0:
+			logger.error('Response data exception (from thinkpage.cn), length: %s, error: %s' % (len(content_from_api), e))
+			self.write(501)
+			return
+		object_from_api = None
+		try:
+			object_from_api = json.loads(content_from_api)
+			if object_from_api is None:
+				raise Exception('object_from_api is None')
+		except Exception, e:
+			logger.error('Response data format exception (from thinkpage.cn), length: %s, error: %s' % (len(content_from_api), e))
+			self.write(501)
+			return
+		status = object_from_api.get('status')
+		if status is None:
+			logger.error('Field \'status\' not found')
+			self.write(501)
+			return
+		if status <> 'OK':
+			logger.error('Service status is not equal to \'OK\' (from thinkpage.cn)')
+			self.write(501)
+			return
+
+		hightemp = None
+		wind = None
+		img = None # TODO
+		weather = None
+		lowtemp = None
+		sksd = None
+		skhour = None
+		skmin = None
+		month = None
+		year = None
+		day = None
+		sktemp = None
+		index_xc = None
+		
+		weather = object_from_api.get('weather')
+		future = None
+		if weather is not None and len(weather) > 0:
+			weather = weather[0]
+			now = weather.get('now')
+			if now is not None:
+				sksd = now.get('humidity')
+			future = weather.get('future')
+			if future is not None and len(future) > 0:
+				future_0 = future[0]
+				hightemp = future_0.get('high')
+				wind = future_0.get('wind')
+				weather = future_0.get('text')
+				lowtemp = future_0.get('low')
+				
+
+		res = '{ '
+		res_today_section = '"today": { ' +
+			'"hightemp": "%s", ' % hightemp +
+			'"wind": "%s", ' % wind +
+			'"img": "%s", ' % img + # TODO WTF
+			'"weather": "%s", ' % weather +
+			'"lowtemp": "%s", ' % lowtemp +
+			'"sksd": "%s", ' % sksd +
+			'"skhour": %s, ' % skhour +
+			'"skmin": %s, ' % skmin +
+			'"month": %s, ' % month +
+			'"year": %s, ' % year +
+			'"day": %s, ' % day +
+			'"sktemp": %s, ' % sktemp +
+			'"index_xc": "%s"' % index_xc +
+			' }, '
+		res += res_today_section
+		res_future_section = '"future": [ '
+		res_future_section_in = []
+		for _ in futures:
+			res_future_section_in.append('{ ' +
+				'"hightemp": "%s", ' % hightemp +
+				'"wind": "%s", ' % wind +
+				'"img": "%s", ' % img + # TODO WTF
+				'"weather": "%s", ' % weather +
+				'"lowtemp": "%s"' % lowtemp +
+				' }')
+		res_future_section += ','.join(res_future_section_in)
+		res_future_section += ' ], '
+		res += res_future_section
+		res_pm_2_5 = '"pm2_5": %s, ' % pm2_5
+		res += res_pm_2_5
+		res_sutime = '"sutime": %s, ' % sutime
+		res += res_sutime
+		res_city = '"city": "%s"' % city_name
+		res += ' }'
+		self.write(res)
+			
+	
+	def name_parse(self, city_name):
+		return city_name # TODO
+		pass
+		
+
+
+
+
+
+
+
+
+
+	
--- web.py
+++ web.py
@@ -3,6 +3,10 @@
 import sys, os
 import tornado.ioloop
 import tornado.web
+import logging
+import logging.handlers
+
+import vehiclenet.weather
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -25,11 +29,25 @@ settings = {
 
 application = tornado.web.Application([
 	(r"/", DefaultHandler),
+	(r"/weather/findWeather.htm", WeatherHandler),
 ], **settings)
 
 if __name__ == "__main__":
 	if '-d' in sys.argv:
 		deamon()
+	logdir = 'logs'
+	if not os.path.exists(logdir):
+		os.makedirs(logdir)
+	fmt = '%(asctime)s - %(filename)s:%(lineno)s - %(name)s - %(message)s'
+	formatter = logging.Formatter(fmt)
+	handler = logging.handlers.TimedRotatingFileHandler(
+		'logging_', 'H', 1, 120)
+	handler.suffix = '%Y%m%d%H%M.log'
+	handler.setFormatter(formatter)
+	logger = logging.getLogger('web')
+	logger.addHandler(handler)
+	logger.setLevel(logging.DEBUG)
+
 	application.listen(80)
 	print 'Server is running, listening on port 80....'
 	tornado.ioloop.IOLoop.instance().start()
